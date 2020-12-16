2ad9873e8a82f31554730f9c32e2653b517c3437
Home <Goose-kr>
2020-12-16 22:32:15 +0400

    Создан POJO класс Location и начал реализовавать POJO класс Settlement

---
+++ src/main/java/ru/ssau/tk/Goose_kr/Praktika_Krasnykh/Helloworld.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.Goose_kr.Praktika_Krasnykh;
+
+public class Helloworld {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+    }
+}
---
+++ src/main/java/ru/ssau/tk/Goose_kr/Praktika_Krasnykh/Person.java
@@ -0,0 +1,27 @@
+package ru.ssau.tk.Goose_kr.Praktika_Krasnykh;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private  int passportId;
+
+    public String getFirstName(){
+        return firstName= firstName;
+    }
+    public String getLastName(){
+        return lastName = lastName;
+    }
+    public int getPassportId(){
+        return passportId=passportId;
+    }
+   public void setFirstName(String firstName) {
+        this.firstName=firstName;
+    }
+   public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+   public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+}
