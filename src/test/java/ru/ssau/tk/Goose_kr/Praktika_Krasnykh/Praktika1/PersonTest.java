b20d42f1ad6ce95553c8fc7ff916f67939803e1d
Home <Goose-kr>
2021-01-15 02:44:29 +0400

    1.8 и 1.9 и 1.10 и 1.11

---
+++ src/test/java/ru/ssau/tk/Goose_kr/Praktika_Krasnykh/Praktika1/PersonTest.java
@@ -0,0 +1,45 @@
+package ru.ssau.tk.Goose_kr.Praktika_Krasnykh.Praktika1;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+    Person firstObj = new Person();
+    Person secondObj = new Person("Tomas", "Ignatenko");
+    Person thirdObj = new Person(null, null, 123456);
+    Person fourthObj = new Person(123456);
+
+    @Test
+    public void testFirstNameMethods() {
+        secondObj.setFirstName(null);
+        thirdObj.setFirstName("Anna");
+        assertNull(secondObj.getFirstName());
+        assertEquals(thirdObj.getFirstName(), "Anna");
+        assertNull(fourthObj.getFirstName());
+    }
+
+    @Test
+    public void testLastNameMethods() {
+        thirdObj.setLastName("Zubenco");
+        fourthObj.setLastName(null);
+
+        assertNull(firstObj.getLastName());
+        assertEquals(secondObj.getLastName(), "Ignatenko");
+        assertEquals(thirdObj.getLastName(), "Zubenco");
+        assertNull(fourthObj.getLastName());
+    }
+
+    @Test
+    public void testPassportIDMethods() {
+        secondObj.setPassportId(742782);
+        fourthObj.setPassportId(697583);
+
+        assertEquals(firstObj.getPassportId(), 0);
+        assertEquals(secondObj.getPassportId(), 742782);
+        assertEquals(thirdObj.getPassportId(), 123456);
+        assertEquals(fourthObj.getPassportId(), 697583);
+    }
+
+
+}
