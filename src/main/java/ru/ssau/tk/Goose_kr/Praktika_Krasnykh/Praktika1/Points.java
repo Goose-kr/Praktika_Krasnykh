e4b666082423204a819fbddcde7569c2252a3841
Home <Goose-kr>
2021-01-15 01:28:14 +0400

    1.4 и 1.5

---
+++ src/main/java/ru/ssau/tk/Goose_kr/Praktika_Krasnykh/Praktika1/Points.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.Goose_kr.Praktika_Krasnykh.Praktika1;
+
+public class Points {
+    private Points() {
+    }
+
+    public static Point sum(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x + secondObj.x, firstObj.y + secondObj.y, firstObj.z + secondObj.z);
+    }
+
+    public static Point subtract(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x - secondObj.x, firstObj.y - secondObj.y, firstObj.z - secondObj.z);
+    }
+
+    public static Point multiply(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x * secondObj.x, firstObj.y * secondObj.y, firstObj.z * secondObj.z);
+    }
+
+    public static Point divide(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x / secondObj.x, firstObj.y / secondObj.y, firstObj.z / secondObj.z);
+    }
+
+    public static Point enlarge(Point obj, double number) {
+        return new Point(obj.x * number, obj.y * number, obj.z * number);
+    }
+}
